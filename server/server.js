252efb96f51786af1a2d71ce260872f21101fdec
Jaden Perpignan <jperpignan313@icloud.com>
2025-06-12 22:56:46 -0400

    Changes to Backend

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 
 app.use("/api/auth", require("./routes/authRoutes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Start server
 const server = app.listen(PORT, () => {
